51b4827dcc1d2d448ec562aca92a807cf2ac9973
Naoki Kimura <n.kimura.cap@gmail.com>
2020-04-24 16:22:47 +0900

    Migrate to TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-const stream = require('stream');
-const { analyzeCodeStatically, tool: { installGem } } = require('@moneyforward/sca-action-core');
+import stream from 'stream';
+import { analyzeCodeStatically, tool } from '@moneyforward/sca-action-core';
 
-(async () => {
+(async (): Promise<void> => {
   const command = 'brakeman';
   const args = ['--no-progress', '--no-exit-on-warn', '-f', 'json', '.'];
-  const transformer = (() => {
-    const buffers = [];
+  const transformer = ((): stream.Transform => {
+    const buffers: Buffer[] = [];
     return new stream.Transform({
       readableObjectMode: true,
-      transform: function (buffer, encoding, done) {
+      transform: function (buffer, _encoding, done): void {
         buffers.push(buffer);
         done();
       },
-      flush: function (done) {
+      flush: function (done): void {
         const result = JSON.parse(Buffer.concat(buffers).toString());
         console.log(`::debug::Detected ${result.warnings.length} problem(s).`);
         for (const warning of result.warnings) this.push({
@@ -28,7 +28,7 @@ const { analyzeCodeStatically, tool: { installGem } } = require('@moneyforward/s
       }
     });
   })();
-  process.exitCode |= await analyzeCodeStatically(command, args, undefined, [transformer], installGem(true, command));
+  process.exitCode = await analyzeCodeStatically(command, args, undefined, [transformer], tool.installGem(true, command));
 })().catch(reason => {
   console.log(`::error::${String(reason)}`);
   process.exit(1);
