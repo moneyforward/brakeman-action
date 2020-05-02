958312661ffcc37e8c2565511f5f82b82918a6af
Naoki Kimura <n.kimura.cap@gmail.com>
2020-05-02 17:49:45 +0900

    Add postversion script

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,29 @@ function spawn(command, args = [], options) {
   return child;
 }
 
+async function stringify(readStream) {
+  const buffers = [];
+  for await (const buffer of readStream) buffers.push(buffer);
+  return Buffer.concat(buffers).toString().trim();
+}
+
+function substitute(command, args = [], options, stdin) {
+  return new Promise((resolve, reject) => {
+    const child = spawn(command, args, Object.assign({}, options, { stdio: [stdin, 'pipe', 'ignore'] }))
+      .once('error', reject);
+    if (child.stdout === null) return resolve('');
+    stringify(child.stdout).then(resolve).catch(reject);
+  });
+}
+
+exports.postversion = async function postversion () {
+  const revision = await substitute('git', ['rev-list', '--tags', '--max-count=1']);
+  const latest = await substitute('git', ['describe', '--tags', revision]);
+  if (!/^v\d+\.\d+\.\d+$/.test(latest)) return;
+  await substitute('git', ['tag', '-f',latest.substring(0, 2), revision]);
+  await substitute('git', ['tag', '-f', latest.substring(0, 4), revision]);
+}
+
 exports['transpile:ncc'] = function ncc() {
   return spawn('ncc', ['build', './src/index.ts', '-m']);
 }
